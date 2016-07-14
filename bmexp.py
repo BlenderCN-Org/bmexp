7b6f84fb96751e78ca7ecb386602a0af56e8e5b7
metropolik <metropolik@web.de>
2016-07-14 22:01:15 +0200

    Seperated uv processing info

--- bmexp.py
+++ bmexp.py
@@ -6,13 +6,22 @@ from mathutils import Vector
 from functools import reduce
 
 d16 = lambda x: x * 16.0
-m = Matrix([[0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]]) #swap y and z
+
+def index(li, ele):
+	for i, e in enumerate(li):
+		if (e[0] == ele[0] and e[1] == ele[1]):
+			return i
+	raise Exception("Could not find " + str(ele) + " in " + str(li))
+
 fd = { (0, -1, 0) : 'down',
 		(0, 1, 0) : 'up',
 		(-1, 0, 0) : 'west',
 		(1, 0, 0) : 'east',
 		(0, 0, -1) : 'north',
 		(0, 0, 1) : 'south' }
+
+fsolver = { (0, 2) : (90, False, True),
+			(1, 3) : (90, False, False)}
 
 def processFace(f):
 	global out, uv_layer
@@ -36,11 +45,31 @@ def processFace(f):
 	face.fro = co[0]
 	face.to = co[-1]
 	face.nor = f.normal	
-	uvl = {} #create uv lookup table containing pairs of bmvert : uv-coord as list	
+	uvl = {} #create uv lookup table containing pairs of bmvert : uv-coord as list
+	uli = []
 	for loop in f.loops:
-		uvl[loop.vert] = list(loop[uv_layer].uv)	
+		t = list(map(lambda x: round(x, 3), list(loop[uv_layer].uv)))
+		uvl[loop.vert] = t
+		uli.append(t)	
+	xs = list(map(lambda x: x[0], uli)) #is sorted CW starting from top left corner
+	ys = list(map(lambda y: y[1], uli))
+	t = max(ys)
+	b = min(ys)
+	l = min(xs)
+	r = max(xs)	
+	uli = [[l, b], [l, t], [r, t], [r, b]]	
+
 	uv = list(map(d16, uvl[face.fro] + uvl[face.to]))
-	face.uv = [uv[0], 16.0 - uv[1], uv[2], 16.0 - uv[3]]	
+	
+	froUvIndex = index(uli, uvl[face.fro])
+	toUvIndex = index(uli, uvl[face.to])
+	print("froUvIndex", froUvIndex, "toUvIndex", toUvIndex)
+
+	face.rot, swapx, swapy = fsolver[(froUvIndex, toUvIndex)]	
+	c0, c1, c2, c3 = 0, 1, 2, 3 #0321
+	if (swapx): c0, c2 = 2, 0
+	if (swapy): c1, c3 = 3, 1
+	face.uv = [uv[c0], 16.0 - uv[c1], uv[c2], 16.0 - uv[c3]]	
 	return face
 
 #face[from vert, to vert, normal, uv[0-3]]
@@ -58,12 +87,11 @@ def export(faces, path):
 		#uv face...
 		uvf = {}
 		sn = fd[tuple(face.nor.normalized().yzx)] #normal in string form #EDIT THIS XYZ!!!! OR fd
-		print("sn", sn)
 		uvf[sn] = {}
 		uvf[sn]['texture'] = '#z00'
 		uvf[sn]['cullface'] = True
-		uvf[sn]['uv'] = face.uv
-
+		uvf[sn]['uv'] = face.uv		
+		uvf[sn]['rotation'] = face.rot		
 		p['faces'] = uvf
 		out['elements'].append(p)
 
